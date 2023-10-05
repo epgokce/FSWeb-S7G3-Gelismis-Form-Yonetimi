b816780bed2923c2c1f94098d0fb1653113f852c
epgokce <ep.gokce@gmail.com>
2023-10-05 09:31:33 +0300

    form

---
+++ kullanici-katilimi/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Kullanıcı Katılımı</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
