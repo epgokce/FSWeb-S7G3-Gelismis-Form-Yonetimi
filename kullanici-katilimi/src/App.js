ffeceece7f11e7e8d360fb4383f01835f4741ea9
epgokce <142578266+epgokce@users.noreply.github.com>
2023-10-06 12:00:00 +0300

    App Update

--- kullanici-katilimi/src/App.js
+++ kullanici-katilimi/src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import Form from './components/Form';
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
+import { useState } from "react";
 
 function App() {
+  const [memberList, setMemberList] = useState([]);
+  const { editMember, setEditMember } = useState(null);
+
+  const addMember = (member) => {
+    const currentMemberList = [...memberList, member];
+    setMemberList(currentMemberList);
+  };
+  const editMemberFn = (updatedMember) => {
+    const updatedMemberList = memberList.map((m) => {
+      if (m.email === editMember.email) {
+        return updatedMember;
+      } else {
+        return m;
+      }
+    });
+    setEditMember(null);
+    setMemberList(updatedMemberList);
+  };
   return (
-    <div>
-      <h1>Kullanıcı Katılımı</h1>
-      <Form />
+    <div className="App">
+      <Form
+        addMember={addMember}
+        editMemberFn={editMemberFn}
+        editMember={editMember}
+      />
+      <h2>Member List</h2>
+      <ul>
+        {memberList.map((member, idx) => (
+          <li key={idx}>
+            {member.isim},<a href={`mailto:${member.email}`}>{member.email}</a>,
+            {member.rol}
+            <button type="button" onClick={() => setEditMember(member)}>
+              Edit
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
